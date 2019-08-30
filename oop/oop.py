4fed00db127a61131948a2cbcd2bd34cb1642f1a
Malako <malako.console2@t-online.de>
2019-08-30 12:38:07 +0200

    Add files via upload

--- oop/oop.py
+++ oop/oop.py
@@ -1,15 +1,38 @@
 from tkinter import *
 import sqlite3
 import time
-
+from tkinter import scrolledtext
 # pass
 
+class ScrolledText(Text):
+    def __init__(self, master=None, **kw):
+        self.frame = Frame(master)
+        self.vbar = Scrollbar(self.frame)
+        self.vbar.pack(side=RIGHT, fill=Y)
 
+        kw.update({'yscrollcommand': self.vbar.set})
+        Text.__init__(self, self.frame, **kw)
+        self.pack(side=LEFT, fill=BOTH, expand=True)
+        self.vbar['command'] = self.yview
+
+        # Copy geometry methods of self.frame without overriding Text
+        # methods -- hack!
+        text_meths = vars(Text).keys()
+        methods = vars(Pack).keys() | vars(Grid).keys() | vars(Place).keys()
+        methods = methods.difference(text_meths)
+
+        for m in methods:
+            if m[0] != '_' and m != 'config' and m != 'configure':
+                setattr(self, m, getattr(self.frame, m))
+
+    def __str__(self):
+        return str(self.frame)
 
 class MainWindow():
+
     def __init__(self, master):
         self.master = master
-        self.master.geometry("1000x700+0+0")
+        self.master.geometry("1000x700+200+200")
         self.master.title("Hausverwaltungs Software")
 
         self.button1 = Button(self.master, text="Neues Haus", fg="black",command=self.new_haus_window).place(x =10, y=20, width=120,height=30)
@@ -26,11 +49,20 @@ class MainWindow():
     def quit(self):
         self.master.destroy()
 
+    def view_db():
+        stext = ScrolledText(bg="white", height=10)
+        stext.place(x=10, y=60)
+        stext.focus_set()
+        stext.mainloop()
+
+
+
 class NeuesHausWindow():
+
     def __init__(self, master):
         self.master=master
 
-        self.master.geometry("500x350+175+250")
+        self.master.geometry("500x350+300+350")
         self.master.title("Neues Haus hinzufügen")
 
         self.label1=Label(self.master, text="Fügen sie ein neues Haus der Datenbank hinzu",fg="black").grid(row=0,column=1)
@@ -44,6 +76,17 @@ class NeuesHausWindow():
         self.label1=Label(self.master, text="Badezimmer :",fg="black").grid(row=8,column=0)
         self.label1=Label(self.master, text="Heizart :",fg="black").grid(row=9,column=0)
         self.label1=Label(self.master, text="Besitzer :",fg="black").grid(row=10,column=0)
+
+        self.label2=Label(self.master, text="",fg="black").grid(row=1,column=0)
+        self.label2=Label(self.master, text="",fg="black").grid(row=2,column=2)
+        self.label2=Label(self.master, text="",fg="black").grid(row=3,column=2)
+        self.label2=Label(self.master, text="in m²",fg="black").grid(row=4,column=2)
+        self.label2=Label(self.master, text="",fg="black").grid(row=5,column=2)
+        self.label2=Label(self.master, text="in €",fg="black").grid(row=6,column=2)
+        self.label2=Label(self.master, text="in m²",fg="black").grid(row=7,column=2)
+        self.label2=Label(self.master, text="",fg="black").grid(row=8,column=2)
+        self.label2=Label(self.master, text="",fg="black").grid(row=9,column=2)
+        self.label2=Label(self.master, text="Vorname / Nachname",fg="black").grid(row=10,column=2)
 
         Haus.farbe=Entry(self.master)
         Haus.farbe.grid(row=1,column=1)
@@ -75,10 +118,12 @@ class NeuesHausWindow():
     def add_new_haus(self):
         Main.new_haus_table_entry(Haus.farbe.get(), Haus.adresse.get(), Haus.baujahr.get(), Haus.wohnflaeche.get(), Haus.zimmer.get(), Haus.preis.get(), Haus.grundstueckgroesse.get(), Haus.badezimmer.get(), Haus.heizart.get(), Haus.besitzer.get())
         time.sleep(.1)
+
         NeuesHausWindow.quit(self)
 
 
 class Haus():
+
     def __init__(self):
         self.farbe = farbe
         self.adresse = adresse
@@ -94,6 +139,7 @@ class Haus():
 class Main():
 
     def connect():
+
         conn = sqlite3.connect("haeuser.db")  # db erstellen
         cur = conn.cursor()  # cursor = cur
 
@@ -104,9 +150,9 @@ class Main():
         cur.execute("CREATE TABLE IF NOT EXISTS HaeuserOne (Farbe text(20), Adresse BLOB, Baujahr INTEGER, Wohnflaeche REAL, Zimmer INTEGER, Preis REAL, Grundstueckgroesse REAL, Badezimmer INTEGER, Heizart TEXT, Besitzer TEXT)")
         # erstellt einen table in der datenbank haeuser.db und definiert die entrys und deren type
 
-
         master = Tk()
         mw = MainWindow(master)
+        MainWindow.view_db()
         master.mainloop()
 
     def new_haus_table_entry(farbe, adresse, baujahr, wohnflaeche, zimmer, preis, grundstueckgroesse, badezimmer, heizart, besitzer): # jener funktions name :D
@@ -118,6 +164,6 @@ class Main():
         # cursor.execute("INSERT INTO PRESSURE (" + city + ") values (?)", (pressure,))
         conn.commit()
 
-
 if __name__ == "__main__":
+
     Main.main()
