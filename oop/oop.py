88192125793586b64e7f52d6c919b2ff7537514c
Malako <malako.console2@t-online.de>
2019-09-02 13:53:54 +0200

    Add files via upload
    einträge können gelöscht werden

--- oop/oop.py
+++ oop/oop.py
@@ -1,32 +1,9 @@
 from tkinter import *
 import sqlite3
+from sqlite3 import Error
 import time
-from tkinter import scrolledtext
 # pass
 
-class ScrolledText(Text):
-    def __init__(self, master=None, **kw):
-        self.frame = Frame(master)
-        self.vbar = Scrollbar(self.frame)
-        self.vbar.pack(side=RIGHT, fill=Y)
-
-        kw.update({'yscrollcommand': self.vbar.set})
-        Text.__init__(self, self.frame, **kw)
-        self.pack(side=LEFT, fill=BOTH, expand=True)
-        self.vbar['command'] = self.yview
-
-        # Copy geometry methods of self.frame without overriding Text
-        # methods -- hack!
-        text_meths = vars(Text).keys()
-        methods = vars(Pack).keys() | vars(Grid).keys() | vars(Place).keys()
-        methods = methods.difference(text_meths)
-
-        for m in methods:
-            if m[0] != '_' and m != 'config' and m != 'configure':
-                setattr(self, m, getattr(self.frame, m))
-
-    def __str__(self):
-        return str(self.frame)
 
 class MainWindow():
 
@@ -36,25 +13,59 @@ class MainWindow():
         self.master.title("Hausverwaltungs Software")
 
         self.button1 = Button(self.master, text="Neues Haus", fg="black",command=self.new_haus_window).place(x =10, y=20, width=120,height=30)
-        self.button2 = Button(self.master, text="Bearbeiten", fg="black",command=self.edit_haus_window).place(x =140, y=20, width=120,height=30)
-        self.button3 = Button(self.master, text="Quit", fg="red",command=self.quit).place(x =870, y=20, width=120,height=30)
+        self.button2 = Button(self.master, text="Bearbeiten", fg="orange",command=self.edit_haus_window).place(x =270, y=20, width=120,height=30)
+        self.button3 = Button(self.master, text="Quit", bg="#38778f" ,fg="red",command=self.quit).place(x =870, y=20, width=120,height=30)
+        self.button4 = Button(self.master, text="Datenbank", fg="blue",command=self.show_db).place(x =140, y=20, width=120,height=30)
+        self.button5 = Button(self.master, text="Löschen", fg="red",command=self.delete).place(x =400, y=20, width=120,height=30)
+
+        self.id = Entry(self.master)
+        self.id.place(x=610,y=25,width=40,height=20)
+        self.label1 = Label(self.master, text="Haus ID:",fg="black").place(x=530,y=25,width=70,height=20)
+
+    def get_user_id(self):
+        userinput = self.id.get()
+        return userinput
 
     def new_haus_window(self):
-        root2=Toplevel(self.master)
-        myGUI=NeuesHausWindow(root2)
+        master2=Toplevel(self.master)
+        nhwGUI=NeuesHausWindow(master2)
 
     def edit_haus_window(self):
-        pass
+        master3=Toplevel(self.master)
+        ehwGUI=EditHausWindow(master3)
+
+
+    def show_db(self):
+        conn = sqlite3.connect("haeuser.db", timeout=2)  # db erstellen und connecten
+        cur = conn.cursor() # cursor erstellen
+
+        cur.execute("SELECT *, oid FROM HaeuserOne")
+        haeuser_in_db = cur.fetchall()
+
+        # print(haeuser_in_db)
+
+        print_db = ""
+        for haeuser in haeuser_in_db:
+            print_db += str(haeuser[10])+ " "+"\t"+ str(haeuser[1])+ " // "+ str(haeuser[9])+"\n"
+
+        show_db_label = Label(self.master, text=print_db)
+        show_db_label.place(x=400,y=350)
+
+        db.conn.commit()
+
+
+    def delete(self):
+        conn = sqlite3.connect("haeuser.db", timeout=2)  # db erstellen und connecten
+        cur = conn.cursor() # cursor erstellen
+        print("lösche eintrag and oid " + self.id.get())
+        try:
+            cur.execute("DELETE from HaeuserOne WHERE oid = " + self.id.get())
+            conn.commit()
+        except Error:
+            print("User Error !!!")
 
     def quit(self):
         self.master.destroy()
-
-    def view_db():
-        stext = ScrolledText(bg="white", height=10)
-        stext.place(x=10, y=60)
-        stext.focus_set()
-        stext.mainloop()
-
 
 
 class NeuesHausWindow():
@@ -77,15 +88,15 @@ class NeuesHausWindow():
         self.label1=Label(self.master, text="Heizart :",fg="black").grid(row=9,column=0)
         self.label1=Label(self.master, text="Besitzer :",fg="black").grid(row=10,column=0)
 
-        self.label2=Label(self.master, text="",fg="black").grid(row=1,column=0)
-        self.label2=Label(self.master, text="",fg="black").grid(row=2,column=2)
-        self.label2=Label(self.master, text="",fg="black").grid(row=3,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=1,column=0)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=2,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=3,column=2)
         self.label2=Label(self.master, text="in m²",fg="black").grid(row=4,column=2)
-        self.label2=Label(self.master, text="",fg="black").grid(row=5,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=5,column=2)
         self.label2=Label(self.master, text="in €",fg="black").grid(row=6,column=2)
         self.label2=Label(self.master, text="in m²",fg="black").grid(row=7,column=2)
-        self.label2=Label(self.master, text="",fg="black").grid(row=8,column=2)
-        self.label2=Label(self.master, text="",fg="black").grid(row=9,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=8,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=9,column=2)
         self.label2=Label(self.master, text="Vorname / Nachname",fg="black").grid(row=10,column=2)
 
         Haus.farbe=Entry(self.master)
@@ -117,9 +128,111 @@ class NeuesHausWindow():
 
     def add_new_haus(self):
         Main.new_haus_table_entry(Haus.farbe.get(), Haus.adresse.get(), Haus.baujahr.get(), Haus.wohnflaeche.get(), Haus.zimmer.get(), Haus.preis.get(), Haus.grundstueckgroesse.get(), Haus.badezimmer.get(), Haus.heizart.get(), Haus.besitzer.get())
-        time.sleep(.1)
+        Haus.farbe.delete(0, END)
+        Haus.adresse.delete(0, END)
+        Haus.baujahr.delete(0, END)
+        Haus.wohnflaeche.delete(0, END)
+        Haus.zimmer.delete(0, END)
+        Haus.preis.delete(0, END)
+        Haus.grundstueckgroesse.delete(0, END)
+        Haus.badezimmer.delete(0, END)
+        Haus.heizart.delete(0, END)
+        Haus.besitzer.delete(0, END)
 
-        NeuesHausWindow.quit(self)
+class EditHausWindow():
+
+    def __init__(self, master):
+
+        conn = sqlite3.connect("haeuser.db", timeout=2)  # db erstellen und connecten
+        cur = conn.cursor() # cursor erstellen
+
+        self.master=master
+
+        self.master.geometry("500x350+300+350")
+        self.master.title("Haus bearbeiten")
+
+        self.label1=Label(self.master, text="Bearbeiten sie ein Haus aus der Datenbank",fg="black").grid(row=0,column=1)
+        self.label1=Label(self.master, text="Farbe :",fg="black").grid(row=1,column=0)
+        self.label1=Label(self.master, text="Adresse :",fg="black").grid(row=2,column=0)
+        self.label1=Label(self.master, text="Baujahr :",fg="black").grid(row=3,column=0)
+        self.label1=Label(self.master, text="Wohnfläche :",fg="black").grid(row=4,column=0)
+        self.label1=Label(self.master, text="Zimmer :",fg="black").grid(row=5,column=0)
+        self.label1=Label(self.master, text="Preis :",fg="black").grid(row=6,column=0)
+        self.label1=Label(self.master, text="Grundstückgröeße :",fg="black").grid(row=7,column=0)
+        self.label1=Label(self.master, text="Badezimmer :",fg="black").grid(row=8,column=0)
+        self.label1=Label(self.master, text="Heizart :",fg="black").grid(row=9,column=0)
+        self.label1=Label(self.master, text="Besitzer :",fg="black").grid(row=10,column=0)
+
+        #self.label2=Label(self.master, text="",fg="black").grid(row=1,column=0)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=2,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=3,column=2)
+        self.label2=Label(self.master, text="in m²",fg="black").grid(row=4,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=5,column=2)
+        self.label2=Label(self.master, text="in €",fg="black").grid(row=6,column=2)
+        self.label2=Label(self.master, text="in m²",fg="black").grid(row=7,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=8,column=2)
+        #self.label2=Label(self.master, text="",fg="black").grid(row=9,column=2)
+        self.label2=Label(self.master, text="Vorname / Nachname",fg="black").grid(row=10,column=2)
+
+
+        Haus.farbe=Entry(self.master)
+        Haus.farbe.grid(row=1,column=1)
+        Haus.adresse=Entry(self.master)
+        Haus.adresse.grid(row=2,column=1)
+        Haus.baujahr=Entry(self.master)
+        Haus.baujahr.grid(row=3,column=1)
+        Haus.wohnflaeche=Entry(self.master)
+        Haus.wohnflaeche.grid(row=4,column=1)
+        Haus.zimmer=Entry(self.master)
+        Haus.zimmer.grid(row=5,column=1)
+        Haus.preis=Entry(self.master)
+        Haus.preis.grid(row=6,column=1)
+        Haus.grundstueckgroesse=Entry(self.master)
+        Haus.grundstueckgroesse.grid(row=7,column=1)
+        Haus.badezimmer=Entry(self.master)
+        Haus.badezimmer.grid(row=8,column=1)
+        Haus.heizart=Entry(self.master)
+        Haus.heizart.grid(row=9,column=1)
+        Haus.besitzer=Entry(self.master)
+        Haus.besitzer.grid(row=10,column=1)
+
+        obj1 = MainWindow()
+        hausid = obj1.get_user_id()
+        cur.execute("SELECT * FROM HaeuserOne WHERE oid = " + hausid)
+        haeuser = cur.fetchall()
+
+
+        for haus in haeuser:
+            Haus.farbe.insert(0, haus[0])
+            Haus.adresse.insert(0, haus[1])
+            Haus.baujahr.insert(0, haus[2])
+            Haus.wohnflaeche.insert(0, haus[3])
+            Haus.zimmer.insert(0, haus[4])
+            Haus.preis.insert(0, haus[5])
+            Haus.grundstueckgroesse.insert(0, haus[6])
+            Haus.badezimmer.insert(0, haus[7])
+            Haus.heizart.insert(0, haus[8])
+            Haus.besitzer.insert(0, haus[9])
+
+        self.button1=Button(self.master, text="Haus speichern",fg="green",command=self.edit_haus).place(x = 20, y= 300, width=100, height=25)
+        self.button1=Button(self.master, text="Abbrechen",fg="red",command=self.quit).place(x = 370, y= 300, width=100, height=25)
+
+    def quit(self):
+        self.master.destroy()
+
+    def edit_haus(self):
+        Main.new_haus_table_entry(Haus.farbe.get(), Haus.adresse.get(), Haus.baujahr.get(), Haus.wohnflaeche.get(), Haus.zimmer.get(), Haus.preis.get(), Haus.grundstueckgroesse.get(), Haus.badezimmer.get(), Haus.heizart.get(), Haus.besitzer.get())
+        Haus.farbe.delete(0, END)
+        Haus.adresse.delete(0, END)
+        Haus.baujahr.delete(0, END)
+        Haus.wohnflaeche.delete(0, END)
+        Haus.zimmer.delete(0, END)
+        Haus.preis.delete(0, END)
+        Haus.grundstueckgroesse.delete(0, END)
+        Haus.badezimmer.delete(0, END)
+        Haus.heizart.delete(0, END)
+        Haus.besitzer.delete(0, END)
+
 
 
 class Haus():
@@ -136,33 +249,40 @@ class Haus():
         self.heizart = heizart
         self.besitzer = besitzer
 
+class db():
+    print("conn to db open")
+    conn = sqlite3.connect("haeuser.db", timeout=2)  # db erstellen und connecten
+    cur = conn.cursor() # cursor erstellen
+
 class Main():
 
-    def connect():
-
-        conn = sqlite3.connect("haeuser.db")  # db erstellen
-        cur = conn.cursor()  # cursor = cur
-
-        return conn, cur
-
     def main():
-        conn, cur = Main.connect()
-        cur.execute("CREATE TABLE IF NOT EXISTS HaeuserOne (Farbe text(20), Adresse BLOB, Baujahr INTEGER, Wohnflaeche REAL, Zimmer INTEGER, Preis REAL, Grundstueckgroesse REAL, Badezimmer INTEGER, Heizart TEXT, Besitzer TEXT)")
+        db.cur.execute("""CREATE TABLE IF NOT EXISTS HaeuserOne (
+            Farbe TEXT(20),
+            Adresse TEXT,
+            Baujahr INTEGER,
+            Wohnflaeche REAL,
+            Zimmer INTEGER,
+            Preis REAL,
+            Grundstueckgroesse REAL,
+            Badezimmer INTEGER,
+            Heizart TEXT,
+            Besitzer TEXT
+            )""")
         # erstellt einen table in der datenbank haeuser.db und definiert die entrys und deren type
+        db.conn.commit()
 
         master = Tk()
         mw = MainWindow(master)
-        MainWindow.view_db()
         master.mainloop()
 
-    def new_haus_table_entry(farbe, adresse, baujahr, wohnflaeche, zimmer, preis, grundstueckgroesse, badezimmer, heizart, besitzer): # jener funktions name :D
-        conn, cur = Main.connect()
-        cur.execute("INSERT INTO HaeuserOne VALUES(?, ?, ?, ? ,?, ?, ?, ?, ?, ?)", (farbe, adresse, baujahr, wohnflaeche, zimmer, preis, grundstueckgroesse, badezimmer, heizart, besitzer))
+    def new_haus_table_entry(farbe, adresse, baujahr, wohnflaeche, zimmer, preis, grundstueckgroesse, badezimmer, heizart, besitzer):
 
-        # cur.execute("INSERT INTO HaeuserOne (%s) VALUES(?)" %(attr),
-                                                     # (data,))
+        db.cur.execute("INSERT INTO HaeuserOne VALUES(?, ?, ?, ? ,?, ?, ?, ?, ?, ?)", (farbe, adresse, baujahr, wohnflaeche, zimmer, preis, grundstueckgroesse, badezimmer, heizart, besitzer))
+        # cur.execute("INSERT INTO HaeuserOne (%s) VALUES(?)" %(attr),(data,))
         # cursor.execute("INSERT INTO PRESSURE (" + city + ") values (?)", (pressure,))
-        conn.commit()
+        db.conn.commit()
+        # db.conn.close()
 
 if __name__ == "__main__":
 
